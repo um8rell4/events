034918cbb32b1c6ee7e2ab9145edcbe65f2f008e
um8re <arkhipka27@gmail.com>
2024-10-20 08:49:14 +0300

    Add postgres DB and create datafile events_data.json

--- event/event/settings.py
+++ event/event/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'event.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'events',
+        'USER': 'postgres',
+        'PASSWORD': 'Saltanov2609-',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
