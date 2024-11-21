0e06e5698aa06f96638a78090cb43b6e41c93d18
um8re <arkhipka27@gmail.com>
2024-11-21 20:34:03 +0300

    last version v3

--- event/organizers/urls.py
+++ event/organizers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import organizer_panel, info_card, publish_review
+from .views import organizer_panel, info_card, publish_review, delete_review
 
 
 urlpatterns = [
     path('', organizer_panel, name='index'),
     path('detail/<int:pk>', info_card, name='info_card'),
     path('detail/publish/<int:pk>', publish_review, name='publish'),
+    path('detail/delete/<int:pk>', delete_review, name='delete'),
 ]
--- event/organizers/views.py
+++ event/organizers/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from users.models import User, UserProfile
 from reviews.models import Review
 from events.models import Event, Booking
+from .forms import EventOrganizerForm
 
 
 @login_required
@@ -16,7 +17,19 @@ def info_card(request, pk):
     event = get_object_or_404(Event, pk=pk)
     reviews = Review.objects.filter(event=event)
     if event.organizer == request.user:
-        return render(request, 'organizers/organizer_event_edit.html', {"event": event, "reviews": reviews})
+        if request.method == 'POST':
+            form = EventOrganizerForm(request.POST)
+            if form.is_valid():
+                organizer_form = form.save(commit=False)
+                organizer_form.event_id = pk
+                organizer_form.save()
+                return redirect(to='users:organizers:info_card', pk=pk)
+            else:
+                print(form.errors)
+        else:
+            form = EventOrganizerForm
+        return render(request, 'organizers/organizer_event_edit.html', {"event": event, "reviews": reviews,
+                                                                        "form": form})
     else:
         return redirect(to='events:events')
 
@@ -28,3 +41,12 @@ def publish_review(request, pk):
         review.is_published = True
         review.save()
     return redirect('users:organizers:info_card', pk=review.event.pk)
+
+
+@login_required
+def delete_review(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == 'POST':
+        review.delete()
+    return redirect('users:organizers:info_card', pk=review.event.pk)
+
